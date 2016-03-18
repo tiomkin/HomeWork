d1bf2c0519a567a9fb7a1b6834fd39f8a8c18119
tiomkin <tiomkin@tut.by>
2016-03-18 14:44:17 +0300

    Added files via upload

---
+++ lesson5/task2/Appliances.java
@@ -0,0 +1,9 @@
+package by.courseproject.lesson5.task2;
+
+/**
+ * Created by Администратор on 18.03.2016.
+ */
+public abstract class Appliances {
+
+    public abstract void on();
+}
---
+++ lesson5/task2/Tv.java
@@ -0,0 +1,12 @@
+package by.courseproject.lesson5.task2;
+
+/**
+ * Created by Администратор on 18.03.2016.
+ */
+public class Tv extends Appliances {
+
+    @Override
+    public void on() {
+        System.out.println("Включили обычный телевизор");
+    }
+}
