e751b22ee59941c209b45bc8a79643a53b6bf105
tiomkin <tiomkin@tut.by>
2016-04-29 10:39:47 +0300

    Update MainActivity.java

--- lesson10/main/java/by/tut/tiomkin/homework10/MainActivity.java
+++ lesson10/main/java/by/tut/tiomkin/homework10/MainActivity.java
@@ -30,13 +30,10 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
     private static int REQUEST_CODE_RECOVER_PLAY_SERVICES = 200;
 
     private static final String TAG = "MainActivity";
-    //private static final String REQUESTING_LOCATION_UPDATES_KEY = "Requesting Location";
 
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation, mCurrentLocation;
     private LocationRequest mLocationRequest;
-    //private boolean mRequestingLocationUpdates = false;
-    //private static final String LOCATION_KEY = "Location Key";
 
     TextView mLatitude, mLongitude;
 
@@ -49,8 +46,6 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
         mLatitude = (TextView) findViewById(R.id.main_ll_tv2_latitude);
         mLongitude = (TextView) findViewById(R.id.main_ll2_tv2_longitude);
 
-        //updateValuesFromBundle(savedInstanceState);
-
         if (checkGooglePlayServices()) {
 
             buildGoogleApiClient();
@@ -58,34 +53,13 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
         }
 
         createLocationRequest();
-
-        /*LocationSettingsRequest.Builder requestBuilder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(mLocationRequest);*/
-
-        //final PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, requestBuilder.build());
-
-        /*result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
-            @Override
-            public void onResult(LocationSettingsResult locationSettingsResult) {
-                final Status status = locationSettingsResult.getStatus();
-                final LocationSettingsStates states = locationSettingsResult.getLocationSettingsStates();
-
-                switch (status.getStatusCode()) {
-
-                    case LocationSettingsStatusCodes.SUCCESS:
-                        createLocationRequest();
-                        break;
-
-                }
-            }
-        });*/
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE_RECOVER_PLAY_SERVICES) {
             if (resultCode == RESULT_OK) {
-                // Make sure the app is not already connected or attempting to connect
+
                 if (!mGoogleApiClient.isConnecting() && !mGoogleApiClient.isConnected()) {
                     mGoogleApiClient.connect();
                 }
@@ -122,21 +96,6 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
 
     }
 
-    /*private void updateValuesFromBundle(Bundle savedInstanceState) {
-        Log.d(TAG, "updateValuesFromBundle()");
-        if (savedInstanceState != null) {
-            if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
-                mRequestingLocationUpdates = savedInstanceState.getBoolean(REQUESTING_LOCATION_UPDATES_KEY);
-            }
-
-            if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
-                mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
-            }
-
-            updateUI();
-        }
-    }*/
-
     private void updateUI() {
         Log.d(TAG, "updateUI()");
         mLatitude.setText(String.valueOf(mLastLocation.getLatitude()));
@@ -146,7 +105,7 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
     @Override
     protected void onStart() {
         Log.d(TAG, "onStart()");
-        //mGoogleApiClient.connect();
+
         super.onStart();
         if (mGoogleApiClient != null) {
             mGoogleApiClient.connect();
@@ -172,9 +131,7 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
     protected void onResume() {
         Log.d(TAG, "onResume()");
         super.onResume();
-        /*if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
-            startLocationUpdates();
-        }*/
+
     }
 
     protected void startLocationUpdates() {
@@ -207,29 +164,7 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
     @Override
     public void onConnected(Bundle bundle) {
         Log.d(TAG, "onConnected()");
-        /*LocationSettingsRequest.Builder requestBuilder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(mLocationRequest);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (mLastLocation != null) {
-            mLatitude.setText(String.valueOf(mLastLocation.getLatitude()));
-            mLongitude.setText(String.valueOf(mLastLocation.getLongitude()));
-        }
 
-        if (mRequestingLocationUpdates) {
-            startLocationUpdates();
-        }*/
-
-        //Вариант из https://www.toptal.com/android/android-developers-guide-to-google-location-services-api
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -279,12 +214,4 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
-    /*@Override
-    protected void onSaveInstanceState(Bundle outState) {
-        Log.d(TAG, "onSaveInstanceState()");
-        outState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY, mRequestingLocationUpdates);
-        outState.putParcelable(LOCATION_KEY, mCurrentLocation);
-        //outState.putString(LAST_UPDATED_TIME_STRING_KEY, mLastUpdatedTime);
-        super.onSaveInstanceState(outState);
-    }*/
 }
